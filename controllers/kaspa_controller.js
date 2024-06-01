def7a1ee4212357e62b04e3ab51a769859b2af54
kamalAa1 <kamal.allcode@gmail.com>
2024-06-02 01:19:29 +0530

    updated

--- controllers/kaspa_controller.js
+++ controllers/kaspa_controller.js
@@ -3,18 +3,18 @@
 const testApi = async (req, res) => {
   res.status(200).json({ status: true, message: "The Api is Working" });
 };
-/// You have to generate wallet from Mnemonic passed in the Body
+///1. You have to generate wallet from Mnemonic passed in the Body
 const generateWalletFromMnemonic = async (req, res) => {
   const { mnemonic } = req.body;
   res.status(200).json({ status: true, message: "Complete The task" });
 };
 
-/// Generate New/Fresh Wallet for Kaspa
+///2. Generate New/Fresh Wallet for Kaspa
 const generateWallet = async (req, res, next) => {
   res.status(200).json({ status: true, message: "Complete The task" });
 };
 
-/// use The Wallet generated in the Second Task , and send Kaspa To Another Kaspa Address
+///3. use The Wallet generated in the Second Task , and send Kaspa To Another Kaspa Address
 /// The fee should be as minimum as possible , and the amount to send should be as small as it can be
 const sendKaspa = async (req, res) => {
   const { amount, to } = req.body;
